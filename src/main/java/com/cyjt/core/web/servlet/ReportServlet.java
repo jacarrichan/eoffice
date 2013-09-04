5dca08f344cd3f363aaf6110821604d4f93256de
jacarrichan <772929989@qq.com>
2013-09-04 09:33:18 +0800

    解决不能使用jetty启动项目的问题
    Signed-off-by: jacarrichan <772929989@qq.com>

---
+++ src/main/java/com/cyjt/core/web/servlet/ReportServlet.java
@@ -0,0 +1,11 @@
+package com.cyjt.core.web.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class ReportServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1245546611589176844L;
+}
